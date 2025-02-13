d47eba7439e361462bd8bf09edc71697e95227ec
vladislavzdanov <vladdbafff@gmail.com>
2025-02-13 19:11:50 +0300

    Initial commit

---
+++ src/main/java/ru/zhdanov/loggingstartergradle/LoggingStarterAutoConfiguration.java
@@ -0,0 +1,9 @@
+package ru.zhdanov.loggingstartergradle;
+
+public class LoggingStarterAutoConfiguration {
+
+    public static void println(String input) {
+        System.out.println("Выведено из gradle стартера: " + input);
+
+    }
+}
